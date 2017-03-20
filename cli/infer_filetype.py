797ad54556b056adee87f8cf3db1351c3d61a2d4
childers <genetics.guy@gmail.com>
2017-03-20 17:40:01 -0400

    implemented test extension first, then check header

--- cli/infer_filetype.py
+++ cli/infer_filetype.py
@@ -1,18 +1,9 @@
-
-
 import os
 import sys
 import argparse
 from clint.textui import puts, indent, colored
+from itertools import islice
 
-# Infer the filetype if not specified.
-# is specified, check if it makes sense
-#Filetypes to use
-# gff3
-# gff2
-# gtf
-# bed
-# sam
 
 def get_ext(file):
     filename, file_extension = os.path.splitext(file)
@@ -20,30 +11,100 @@ def get_ext(file):
         param, value = file_extension.split(".",1)
     return value, filename
 
-def check_gff3_file_format(file):
+def check_header(header):
+    # takes a list and iterates through to find format
+    format = 'NA'
+    for line in header:
+        if header[0]== "##gff-version 3\n":
+            format = 'gff3'
+            break
+#            break            
+        if line.startswith('@'):
+            format = 'sam'
+        elif check_gff3_file_format(header) is True:
+            format = 'gff3'
+        elif check_gtf_file_format(header) is True:
+            format = 'gtf'
+        elif check_bed_file_format(header) is True:
+            format = 'bed'
+    print format
+    return format
+
+
+def check_num_of_columns(header):
+    cols = ''
+    for line in header:
+        if line.startswith("#"):
+            next
+        else:
+            items = line.split("\t")
+            cols = len(items)
+    return cols
+
+def check_gff3_file_format(header):
+    status = False
+    gff3_header = False
+    nine_cols = True
+    cols = check_num_of_columns(header)
+    if header[0] == "##gff-version 3\n":
+        gff3_header = True
+        cols = check_num_of_columns(header)
+        if cols == '9':
+            nine_cols = True
+        if nine_cols == True and gff3_header == True:
+            status = True
+    return status
+
+
+def check_bed_file_format(header):
+    status = False
+    cols = check_num_of_columns(header)
+    print cols
+    if cols >= 3 and cols <= 12:
+        status = True
+    return status
+
+def check_gtf_file_format(header):
+    status = False
+    cols = check_num_of_columns(header)
+    if cols == 9:
+        status = True
+    return status
+
+def get_header(file):
+    head = ''
     print file
     with open(file) as myfile:
-        test_passed = 0
-        head = [next(myfile) for x in xrange(10)]
-        if head[0] == "##gff-version 3":
-            test_passed++
-        
-        for line in head:
-
-        print head
-#One method if the seqID is in column 1
+        head = list(islice(myfile, 100))
+    return head
 
 def infer_type(input_file):
+    header = get_header(input_file)
     ext, file = get_ext(input_file)
     if ext == 'gff' or ext == 'gff3' or ext == 'gtf' or ext =='bed':
         if ext == 'gff3' or ext =='gff':
-            gff3_valid = check_gff3_file_format(input_file)
-            print gff3_valid
-
-        print "%s  extension reports as %s.  Replace IDs in column 1" % (input_file, ext)
-    else:
-        print "Filetype %s is not supported" %(ext)
-
+            gff3_status = check_gff3_file_format(header)
+            if gff3_status == True:
+                print "%s passes as gff.  Replace IDs in column 1" % (input_file)
+        elif ext == 'gtf':
+            gtf_status = check_gtf_file_format(header)
+            if gtf_status is True:
+                print "%s passes as gtf.  Replace IDs in column 1" % (input_file)
+#### Need bedfile for testing
+        elif ext =='bed':
+            print 'bed'
+            check_bed_file_format(header)
+####
+        elif ext =='sam':
+            print 'sam'
+#        else:
+#            print "Filetype %s is not supported" %(ext)                
+        header_format = check_header(header)
+        if header_format == "NA":
+            print error
+        else:
+            print "processing header"
+            print header_format
 
 def main(args = sys.argv[1:]):
     parser = argparse.ArgumentParser()
