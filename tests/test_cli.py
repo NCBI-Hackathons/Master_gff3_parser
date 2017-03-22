2c588060c59d9b8ffa93478277c6bf0c1f6212cc
manny809dr <manny809dr@gmail.com>
2017-03-22 16:21:45 -0400

    Addded two cases to test_cli.py

--- tests/test_cli.py
+++ tests/test_cli.py
@@ -1,6 +1,6 @@
 from cli import command
 from tests.test_utilities import Capturing
-from cli.assembly import fetch_assembly_report, get_mapper
+from cli.assembly import fetch_assembly_report, get_mapper, convert, converter
 
 
 def test_fetch_GRCh38():
@@ -12,7 +12,12 @@ def test_fetch_GRCh38():
 
 def test_get_mapper():
     assert get_mapper("http://ftp.ncbi.nlm.nih.gov/genomes/all/GCF/000/001/405/GCF_000001405.26_GRCh38/GCF_000001405.26_GRCh38_assembly_report.txt")
-  
+
+def test_convert():
+	assert convert()
+
+def test_converter():
+	assert converter() 
 """
 def test_vars():
     with Capturing() as out:
