340251e5228ab5b4c5d114be26f23131a39ad71f
childers <genetics.guy@gmail.com>
2017-03-22 15:04:31 -0400

    adding sample convert commands

---
+++ tests/test_data/NCBI/test_convert_commands.sh
@@ -0,0 +1,4 @@
+time seqconv convert  --out gb --ref Mm_Celera alt_Mm_Celera_top_level.gff3 >mouse_Celera_nogz.gff3
+time seqconv convert  --out gb --ref Mm_Celera alt_Mm_Celera_top_level.gff3.gz >mouse_Celera_gz.gff3
+
+
